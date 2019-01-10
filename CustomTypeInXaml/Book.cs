684ec1e2597a3581083517010e950f5632b52c4c
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-01-10 16:01:42 +0530

    Initial commit

---
+++ CustomTypeInXaml/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://www.dileepbalakrishnan.com", "CustomTypeInXaml")]
+
+namespace CustomTypeInXaml
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public int YearPublished { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} by {Author} published on {YearPublished}";
+        }
+    }
+}
---
+++ DependencyObjectDemo/Properties/MySimpleControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DependencyObjectDemo.Properties
+{
+    /// <summary>
+    ///     Interaction logic for MySimpleControl.xaml
+    /// </summary>
+    public partial class MySimpleControl : UserControl
+    {
+        // Using a DependencyProperty as the backing store for YYearPublished.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty YearPublishedProperty =
+            DependencyProperty.Register("YearPublished", typeof(int), typeof(MySimpleControl),
+                new PropertyMetadata(2000));
+
+        public MySimpleControl()
+        {
+            InitializeComponent();
+        }
+
+
+        public int YearPublished
+        {
+            get => (int) GetValue(YearPublishedProperty);
+            set => SetValue(YearPublishedProperty, value);
+        }
+    }
+}
