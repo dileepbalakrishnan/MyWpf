a3ce214d8d9a7ea0b8dc6c6ab830262b16d99ad9
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-01-16 23:08:25 +0530

    Demonstrate custom markup extension.

---
+++ CustomMarkupExtension/RandomExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace CustomMarkupExtension
+{
+    public class RandomExtension : MarkupExtension
+    {
+        private readonly int _from;
+        private readonly Random _rnd = new Random();
+        private readonly int _to;
+
+        public RandomExtension(int from, int to)
+        {
+            _from = from;
+            _to = to;
+        }
+
+        public RandomExtension(int to) : this(0, to)
+        {
+        }
+
+        public bool UseFractions { get; set; }
+        /// <summary>
+        /// Returns a random number after converting it into a suitable type.
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var value = UseFractions
+                ? _rnd.NextDouble() * (_to - _from) + _from
+                : _rnd.Next(_from, _to);
+            Type targetType = null;
+            if (serviceProvider != null)
+                if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget target)
+                    switch (target.TargetProperty)
+                    {
+                        case PropertyInfo clrProp:
+                            targetType = clrProp.PropertyType;
+                            break;
+                        case DependencyProperty dp:
+                            targetType = dp.PropertyType;
+                            break;
+                    }
+            return targetType != null
+                ? Convert.ChangeType(value, targetType)
+                : value.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
