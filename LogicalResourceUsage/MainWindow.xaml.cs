eff34a33412850287fe81e9a46b80b5bf36c3211
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-01-19 14:43:06 +0530

    Demonstrate static and dynamic resource usage.

---
+++ LogicalResourceUsage/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace LogicalResourceUsage
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonBase_OnClick3(object sender, RoutedEventArgs e)
+        {
+            var brush2 = (Brush)TryFindResource("brush2");
+            if (brush2 != null)
+            {
+                Resources["brush3"] = brush2;
+            }
+        }
+    }
+}
