a4f0e5e598eec5f509079488ef3edb79783a4b71
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-01-20 16:04:28 +0530

    Demonstrate the usage of uniform grid.

---
+++ UniformGridDemo/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace UniformGridDemo
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateListbox();
+        }
+
+        private void PopulateListbox()
+        {
+            for (var i = 0; i < 100; i++)
+                lst1.Items.Add(i);
+        }
+    }
+}
