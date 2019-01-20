6c2a3315e33f6a0573c4c8538c8db4a782d89efb
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-01-20 15:44:31 +0530

    Demonstrate the usage of IsSharedSizeScope property

---
+++ SharedRowColumnSizeDemo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace SharedRowColumnSizeDemo
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateListBoxes();
+        }
+
+        private void PopulateListBoxes()
+        {
+            var people = new List<Person>
+            {
+                new Person
+                {
+                    Name = "Dileep",
+                    Age = 35
+                },
+                new Person
+                {
+                    Name = "Bindu",
+                    Age = 32
+                },
+                new Person
+                {
+                    Name = "Kukkudu",
+                    Age = 2
+                }
+            };
+
+            people.ForEach(p =>
+            {
+                lst1.Items.Add(p);
+                lst2.Items.Add(p);
+            });
+        }
+    }
+}
