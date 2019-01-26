b535a7522f30e8589e477fa5d19c6db9c1d4b92e
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-01-27 01:45:26 +0530

    Demonstrate Data Template

---
+++ DataTemplateDemo/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataTemplateDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new Person
+            {
+                Name = "Dileep",
+                Age = 35
+            };
+            _lstPeople.ItemsSource = new List<Person>
+            {
+                new Person
+                {
+                    Name = "Dileep",
+                    Age = 35
+                },
+                new Person
+                {
+                    Name = "Bindu",
+                    Age = 35
+                },
+                new Person
+                {
+                    Name = "Manu",
+                    Age = 35
+                }
+        };
+        }
+    }
+}
---
+++ DataTemplateDemo/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace DataTemplateDemo
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int _age;
+
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                _age = value;
+                var pc = PropertyChanged;
+                pc?.Invoke(this, new PropertyChangedEventArgs(nameof(Age)));
+            }
+        }
+    }
+}
