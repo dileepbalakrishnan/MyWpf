433bfc580c57d82c91d07ced552191da7f04fa6d
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-01-26 18:27:53 +0530

    Change notification demo

---
+++ ChangeNotificationDemo/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChangeNotificationDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person p = new Person { Name = "Dileep", Age = 35 };
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = p;
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            p.Age++;
+        }
+    }
+}
