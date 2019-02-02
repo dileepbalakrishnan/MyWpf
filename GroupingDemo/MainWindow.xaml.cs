5d2ff25a4986abf35f93681c1602db38817da25c
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-02-02 19:38:44 +0530

    Demonstrate grouping using ICollectionView

---
+++ GroupingDemo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroupingDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = Process.GetProcesses().Where(CanAccess);
+            var view = CollectionViewSource.GetDefaultView(DataContext);
+            view.GroupDescriptions.Add(new PropertyGroupDescription("PriorityClass"));
+        }
+
+        public static bool CanAccess(Process process)
+        {
+            try
+            {
+                var h = process.Handle;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
