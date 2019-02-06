747adc7b305313f1722481af95833e13eed5c340
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-02-06 16:11:26 +0530

    Demonstrate multibinding.

---
+++ MultiBindingDemo/RgbConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MultiBindingDemo
+{
+    public class RgbConverter : IMultiValueConverter
+    {
+        private readonly SolidColorBrush _brush = new SolidColorBrush();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = Color.FromRgb(System.Convert.ToByte(values[0]), System.Convert.ToByte(values[1]),
+                System.Convert.ToByte(values[2]));
+            _brush.Color = color;
+            return _brush;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
